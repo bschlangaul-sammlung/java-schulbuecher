4fd7b5e6da429e4de31537d6019bed3c2e5a1c4d
Hermine Bschlangaul <hermine.bschlangaul@gmx.net>
2024-12-15 17:14:04 +0100

    Abituraufgabe

---
+++ src/main/java/org/bschlangaul/schulbuecher/isb/abitur/pruefung_2012/inf1/aufgabe_ii/teilaufgabe_03/BaumElement.java
@@ -0,0 +1,8 @@
+package org.bschlangaul.schulbuecher.isb.abitur.pruefung_2012.inf1.aufgabe_ii.teilaufgabe_03;
+
+public abstract class BaumElement
+{
+
+    abstract Kunde kundeSuchen(int kdnr);
+
+}
---
+++ src/main/java/org/bschlangaul/schulbuecher/isb/abitur/pruefung_2012/inf1/aufgabe_ii/teilaufgabe_03/Kunde.java
@@ -0,0 +1,11 @@
+package org.bschlangaul.schulbuecher.isb.abitur.pruefung_2012.inf1.aufgabe_ii.teilaufgabe_03;
+
+public class Kunde
+{
+
+    int kundennummerGeben()
+    {
+        return 0;
+    }
+
+}
