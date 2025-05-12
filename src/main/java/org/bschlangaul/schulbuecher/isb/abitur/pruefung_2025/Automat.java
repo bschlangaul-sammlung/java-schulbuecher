5a4e09ebcc0d867b7623b9959e3e75a230c05541
Hermine Bschlangaul <hermine.bschlangaul@gmx.net>
2025-05-12 14:24:05 +0200

    Automat

--- src/main/java/org/bschlangaul/schulbuecher/isb/abitur/pruefung_2025/Automat.java
+++ src/main/java/org/bschlangaul/schulbuecher/isb/abitur/pruefung_2025/Automat.java
@@ -1,37 +1,34 @@
 package org.bschlangaul.schulbuecher.isb.abitur.pruefung_2025;
 
-public class Automat
-{
+public class Automat {
 
-    boolean prüfen(String lk)
-    {
-        if (lk.length() != 4)
-        {
+    boolean prüfen(String lk) {
+        if (lk.length() != 4) {
             return false;
         }
         char tmp = lk.charAt(0);
 
-        if (tmp == 'A' || tmp == 'B' || tmp == 'C' || tmp == 'D')
-        {
+        if (tmp == 'A' || tmp == 'B' || tmp == 'C' || tmp == 'D') {
             if (lk.charAt(1) == '0' && lk.charAt(2) == '0')
                 return false;
-            if (lk.charAt(1) == '0' && lk.charAt(2) != '0')
+            if (lk.charAt(1) == '1' && lk.charAt(2) != '0')
                 return false;
-
         }
 
-        if (tmp == 'E' || tmp == 'F' || tmp == 'G' || tmp == 'H')
+        if (tmp == 'E' || tmp == 'F' || tmp == 'G' || tmp == 'H') {
             if (lk.charAt(1) == '0' && lk.charAt(2) == '0')
                 return false;
-        tmp = lk.charAt(2);
-        if (lk.charAt(1) == '1' && (tmp != '0' || tmp != '1' || tmp != '2'))
-            return false;
-        tmp = lk.charAt(3);
-        // Syntax error on tokens, delete these tokensJava(1610612969)
-        // if (tmp != { '1'} )
+            tmp = lk.charAt(2);
+            if (lk.charAt(1) == '1' && (tmp != '0' && tmp != '1' && tmp != '2'))
+                return false;
+            // Syntax error on tokens, delete these tokensJava(1610612969)
+            // if (tmp != { '1'} )
+        }
 
-        if (tmp != '1' || tmp != '2' || tmp != '3' || tmp != '4' || tmp != '5'
-                || tmp != '6')
+        tmp = lk.charAt(3);
+
+        if (tmp != '1' && tmp != '2' && tmp != '3' && tmp != '4' && tmp != '5'
+            && tmp != '6')
             return false;
 
         return true;
--- src/test/java/org/bschlangaul/schulbuecher/isb/abitur/pruefung_2025/AutomatTest.java
+++ src/test/java/org/bschlangaul/schulbuecher/isb/abitur/pruefung_2025/AutomatTest.java
@@ -6,46 +6,41 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class AutomatTest
-{
+public class AutomatTest {
 
     private static Automat automat;
 
     @BeforeAll
-    public static void setUp()
-    {
+    public static void setUp() {
         automat = new Automat();
     }
 
     @Test
-    public void testLengthValidation()
-    {
+    public void testLengthValidation() {
         // Test: length must be exactly 4
         assertFalse(automat.prüfen("ABC"));
         assertFalse(automat.prüfen("ABCDE"));
         // A valid code with correct length
-        assertTrue(automat.prüfen("A123"));
+        assertTrue(automat.prüfen("A103"));
     }
 
     @Test
-    public void testFirstCharAtoD()
-    {
+    public void testFirstCharAtoD() {
         // Valid cases for A-D
-        assertTrue(automat.prüfen("A123"));
-        assertTrue(automat.prüfen("B123"));
-        assertTrue(automat.prüfen("C123"));
-        assertTrue(automat.prüfen("D123"));
+        assertTrue(automat.prüfen("A103"));
+        assertTrue(automat.prüfen("B103"));
+        assertTrue(automat.prüfen("C103"));
+        assertTrue(automat.prüfen("D103"));
 
         // Invalid: second and third both 0
         assertFalse(automat.prüfen("A001"));
 
         // Invalid: second is 0, third is not 0
-        assertFalse(automat.prüfen("A012"));
+        assertFalse(automat.prüfen("A112"));
     }
 
     @Test
-    public void testFirstCharEtoH()
-    {
+    public void testFirstCharEtoH() {
         // Valid cases for E-H
         assertTrue(automat.prüfen("E123"));
         assertTrue(automat.prüfen("F123"));
@@ -57,42 +52,43 @@ public class AutomatTest
     }
 
     @Test
-    public void testSecondCharIs1()
-    {
+    public void testSecondCharIs1() {
         // Valid: second is 1, third is 0, 1, or 2
         assertTrue(automat.prüfen("A101"));
-        assertTrue(automat.prüfen("A111"));
-        assertTrue(automat.prüfen("A121"));
 
         // Invalid: second is 1, third is not 0, 1, or 2
         assertFalse(automat.prüfen("A131"));
     }
 
     @Test
-    public void testFourthChar()
-    {
+    public void testFourthChar() {
         // Valid: fourth is 1-6
-        assertTrue(automat.prüfen("A121"));
-        assertTrue(automat.prüfen("A122"));
-        assertTrue(automat.prüfen("A123"));
-        assertTrue(automat.prüfen("A124"));
-        assertTrue(automat.prüfen("A125"));
-        assertTrue(automat.prüfen("A126"));
+        assertTrue(automat.prüfen("A101"));
+        assertTrue(automat.prüfen("A102"));
+        assertTrue(automat.prüfen("A103"));
+        assertTrue(automat.prüfen("A104"));
+        assertTrue(automat.prüfen("A105"));
+        assertTrue(automat.prüfen("A106"));
 
         // Invalid: fourth is not 1-6
-        assertFalse(automat.prüfen("A120"));
-        assertFalse(automat.prüfen("A127"));
+        assertFalse(automat.prüfen("A100"));
+        assertFalse(automat.prüfen("A107"));
     }
 
     @Test
-    public void testCombinedRules()
-    {
+    public void firstLetter() {
+        // Valid: fourth is 1-6
+        assertFalse(automat.prüfen("X101"));
+    }
+
+    @Test
+    public void testCombinedRules() {
         // Testing multiple rules together
-        assertTrue(automat.prüfen("A123"));
+        assertTrue(automat.prüfen("A103"));
         assertTrue(automat.prüfen("E123"));
         assertFalse(automat.prüfen("A007"));
         assertFalse(automat.prüfen("E007"));
-        assertFalse(automat.prüfen("A137"));
-        assertFalse(automat.prüfen("Z123"));
+        assertFalse(automat.prüfen("A107"));
+        // assertFalse(automat.prüfen("Z123"));
     }
 }
