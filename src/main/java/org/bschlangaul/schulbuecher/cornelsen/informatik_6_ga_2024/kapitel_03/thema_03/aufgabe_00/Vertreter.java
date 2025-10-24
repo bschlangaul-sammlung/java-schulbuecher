6c16511685dfe29c96c44418f0d5ed13dab4ec87
Hermine Bschlangaul <hermine.bschlangaul@gmx.net>
2025-10-24 22:40:38 +0200

    Add cornelsen.informatik_6_ga_2024.kapitel_03.thema_03.aufgabe_00

---
+++ src/main/java/org/bschlangaul/schulbuecher/cornelsen/informatik_6_ga_2024/kapitel_03/thema_03/aufgabe_00/Vertreter.java
@@ -0,0 +1,33 @@
+package org.bschlangaul.schulbuecher.cornelsen.informatik_6_ga_2024.kapitel_03.thema_03.aufgabe_00;
+
+/**
+ * Rahmenprogramm für die Vertreterreise
+ *
+ * @author Albert Wiedemann
+ * @version 1.0
+ */
+class Vertreter
+{
+    GraphMatrix g;
+    Lesen l;
+
+    /**
+     * Legt das Graphenobjekt an.
+     */
+    Vertreter()
+    {
+        g = new GraphMatrix();
+        l = new Lesen();
+        l.LesenDatenbank("Autobahn.grdb", g);
+    }
+
+    /**
+     * Startet die Besuche der Knoten.
+     * @param start Name des Startknotens
+     */
+    void ReiseStarten(String start)
+    {
+        g.ZurückSetzen();
+        g.KnotenBesuchen(g.KnotenNummerGeben(start));
+    }
+}
